1b70532e488709c45b2a917bdd5ef9fecfcb1c46
Idowu Daramola <idowu.daramola@gmail.com>
2016-03-04 15:02:17 +0000

    added readme documentation and added jasmine test

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
-# oneCredit
+A rating app
+author: idowu daramola
+location: lagos
+address: idowu.daramola@gmail.com
 
-a [Sails](http://sailsjs.org) application
+
+Notes for developers that may want to extend this app:
+
+This is a simple NodeJs application using SailsJs as the server and AngularJs for client-side manipulations.
+The Sails server serve static files from the assets/ directory. No server-side copmilation of views is required.
+You are invited to extend the app by creating dynamic views using EJS.
+The two major files, apart from linked external files, that makeup the app can be found in assets/index.html and assets/js/onecredit.js.
+
+Tests for application are written in Jasmine and can be found in assets/js/onecredit.js
+
+
+
+Notes for users
+
+The simply displays a list of books and rating scale for each book.
+The books are displayed with the same default average rating of 20%.
+However users are free rate each book.
+Use and share alike
--- assets/js/onecredit.js
+++ assets/js/onecredit.js
@@ -1,5 +1,5 @@
-angular.module('ui.bootstrap.demo', ['ngAnimate', 'ui.bootstrap']);
-angular.module('ui.bootstrap.demo').controller('RatingDemoCtrl',['$scope', function ($scope) {
+angular.module('onecredit', ['ngAnimate', 'ui.bootstrap']);
+angular.module('onecredit').controller('RatingController',['$scope', function ($scope) {
   $scope.rate = 2;
   $scope.max = 5;
   $scope.isReadonly = false;
@@ -17,3 +17,38 @@ angular.module('ui.bootstrap.demo').controller('RatingDemoCtrl',['$scope', funct
     {stateOff: 'glyphicon-off'}
   ];
 }]);
+
+// Test specs written Jasmine to test RatingDemoCtrl controller
+
+describe('RatingController', function() {
+  beforeEach(module('onecredit'));
+
+  var $controller;
+
+  beforeEach(inject(function(_$controller_){
+    $controller = _$controller_;
+  }));
+
+    it('rates books from 1 star to 5 stars', function() {
+      var $scope = {};
+      var controller = $controller('RatingController', { $scope: $scope });
+      $scope.rate = 2;
+      $scope.max = 5;
+      $scope.isReadonly = false;
+      $scope.hoveringOver(v);
+      $scope.ratingStates = [
+    {stateOn: 'glyphicon-ok-sign', stateOff: 'glyphicon-ok-circle'},
+    {stateOn: 'glyphicon-star', stateOff: 'glyphicon-star-empty'},
+    {stateOn: 'glyphicon-heart', stateOff: 'glyphicon-ban-circle'},
+    {stateOn: 'glyphicon-heart'},
+    {stateOff: 'glyphicon-off'}
+  ];
+      expect($scope.overStar).toEqual(v);
+      expect($scope.rate).toBe(2);
+      expect($scope.max).toEqual(5);
+      expect($scope.isReadonly).not.toBeTruthy();
+      expect($scope.ratingStates).toContain({stateOff: 'glyphicon-off'});
+
+
+    });
+});
